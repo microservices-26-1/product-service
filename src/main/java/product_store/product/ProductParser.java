a99012947e0b09e11d5a5b0a56baee153f603c72
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2026-05-28 13:24:37 -0300

    fix: add id to ProductParser

--- src/main/java/product_store/product/ProductParser.java
+++ src/main/java/product_store/product/ProductParser.java
@@ -4,14 +4,16 @@ public class ProductParser {
 
     public static ProductDTO to(Product product) {
         return ProductDTO.builder()
+            .id(product.id())
             .name(product.name())
             .price(product.price())
             .unit(product.unit())
             .build();
-    }
-
-    public static Product to(ProductDTO dto) {
-        return Product.builder()
+        }
+        
+        public static Product to(ProductDTO dto) {
+            return Product.builder()
+            .id(dto.id())
             .name(dto.name())
             .price(dto.price())
             .unit(dto.unit())
